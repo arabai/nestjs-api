55fc7a8e430cdc86a9a58ab720f2c2ffa3ca6476
Anas Rabai <rabaianas@yahoo.it>
2022-11-05 12:45:59 +0100

    Movies dependencies moved to the Movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, MoviesService],
+  imports: [MoviesModule],
 })
 export class AppModule {}
