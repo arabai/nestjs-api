306fd87f27188dc806a82e18312d5e5e29f5f9b0
Anas Rabai <rabaianas@yahoo.it>
2022-11-05 17:27:48 +0100

    Movies service and controller MongoDB integration

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,33 +1,34 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { MoviesService } from './movies.service';
+import { Movie } from './interfaces/movie.interface';
 
 @Controller('movies')
 export class MoviesController {
     constructor(private moviesService: MoviesService) {}
     
     @Get()
-    getAllMovies(): string {
+    getAllMovies(): Promise<Movie[]> {
         return this.moviesService.findAllMovies()
     }
 
     @Get('/:id')
-    getOneMovie(@Param('id') id:number): string {
+    getOneMovie(@Param('id') id: string): Promise<Movie> {
         return this.moviesService.findOneMovie(id)
     }
 
     @Post()
-    postOneMovie(@Body() createMovieDto: CreateMovieDto): CreateMovieDto {
+    postOneMovie(@Body() createMovieDto: CreateMovieDto): Promise<Movie> {
         return this.moviesService.addOneMovie(createMovieDto)
     }
 
     @Put('/:id')
-    putOneMovie(@Param('id') id:number, @Body() createMovieDto: CreateMovieDto): CreateMovieDto {
+    putOneMovie(@Param('id') id: string, @Body() createMovieDto: CreateMovieDto): Promise<Movie> {
         return this.moviesService.editOneMovie(id, createMovieDto)
     }
 
     @Delete('/:id')
-    deleteOneMovie(@Param('id') id:number, @Body() createMovieDto: CreateMovieDto): CreateMovieDto {
-        return this.moviesService.removeOneMovie(id, createMovieDto)
+    deleteOneMovie(@Param('id') id: string): Promise<Movie> {
+        return this.moviesService.removeOneMovie(id)
     }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,27 +1,32 @@
-import { Body, Injectable, Param } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable, Param } from '@nestjs/common';
+import { Movie, MovieDocument } from './schemas/movie.schema';
 import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
 export class MoviesService {
+    constructor(@InjectModel(Movie.name) private movieModel: Model<MovieDocument>) {}
 
-    findAllMovies(): string {
-        return "All movies returned"
+    async findAllMovies(): Promise<Movie[]> {
+        return await this.movieModel.find();
     }
 
-    findOneMovie(@Param('id') id:number): string {
-        return `Movie with id: ${id} returned`
+    async findOneMovie(@Param('id') id: string): Promise<Movie> {
+        return await this.movieModel.findById(id);
     }
 
-    addOneMovie(@Body() createMovieDto : CreateMovieDto) : CreateMovieDto {
-        return createMovieDto
+    async addOneMovie(createMovieDto : CreateMovieDto): Promise<Movie> {
+        const createdMovie = new this.movieModel(createMovieDto);
+        return await createdMovie.save();
     }
 
-    editOneMovie(@Param('id') id:number, @Body() createMovieDto : CreateMovieDto) : CreateMovieDto {
-        return createMovieDto
+    async editOneMovie(@Param('id') id: string, createMovieDto: CreateMovieDto): Promise<Movie> {
+        return await this.movieModel.findByIdAndUpdate(id, createMovieDto);
     }
 
-    removeOneMovie(@Param('id') id:number, @Body() createMovieDto : CreateMovieDto) :  CreateMovieDto {
-        return createMovieDto
+    async removeOneMovie(@Param('id') id: string): Promise<Movie> {
+        return await this.movieModel.findByIdAndRemove(id);
     }
 
 }
